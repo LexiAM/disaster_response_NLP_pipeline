a377c8c64e84061d88e8d6f2f2334c359b457fa3
AlexanderM1 <manasson.a@gmail.com>
2020-01-13 14:06:25 -0500

    feat: /models/train_classifier.py
    Add python script for model training.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,118 @@
 import sys
+import sqlite3
+import pickle
+
+import pandas as pd
+
+# NLP packages
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+# Model training and evaluation
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import ComplementNB
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, hamming_loss
 
 
 def load_data(database_filepath):
-    pass
+    """ Load data from 'message' table of the input SQLite database.
+    
+    Args:
+        database_filepath (str): path to SQLite database file
+
+    Returns:
+        X (pd.DataFrame): features
+        Y (pd.DataFrame): targets
+        category_names ([str]): list of message category names
+    """
+    conn = sqlite3.connect(database_filepath)
+    df = pd.read_sql_query('SELECT * FROM message', conn, index_col='id')
+    X = df.message
+    Y = df.loc[:, 'related':]
+    category_names = Y.columns.values
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    """ Return lowercase lemmatized tokens of the input text. 
+        Remove punctuation, spacy.stop_words, and pronouns.
+    
+    Args:
+        text (str): input text
+    Returns:
+        tokens ([str]): list of lower case lemmatized tokens
+    """
+    
+    doc = nlp(text)
+    tokens = [tok.lemma_.lower() for tok in doc
+              if not (tok.is_stop or tok.is_punct or (tok.lemma_ == '-PRON-'))]
+    return tokens
 
 
 def build_model():
-    pass
+    """ Build ML pipeline for message classification. 
+        Model parameter tuning can be found in 
+        '../model/ML Pipeline Preparation.ipynb'
+
+    Args:
+        None
+    Returns:
+        model (sklearn.pipeline)    
+    """
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize, ngram_range=(1, 2))),
+        ('tfidf', TfidfTransformer(norm='l1', use_idf=False)),
+        ('clf', MultiOutputClassifier(ComplementNB(alpha=0.1)))
+    ])
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """ Print message classification model evaluation report.
+            For each category:
+            - Precision
+            - Recall
+            - F1 score
+            - Support
+
+            Summary:
+                - Micro avg for each score type
+                - Macro avg for each score type
+                - Weighted avg for each score type
+                - Samples avg for each score type
+                - Hamming Loss
+    
+    Args:
+        model (sklearn.estimator): trained multi-output classifier model
+        X_test (pd.DataFrame): test set features
+        Y_test (pd.DataFrame): test set targets (all categories)
+        category_names ([str]): list of classification category names
+
+    Returns:
+        None
+    """
+    preds = pd.DataFrame(model.predict(X_test), columns=category_names)
+    report = classification_report(Y_test, preds, 
+                                   target_names=category_names)
+    print(report)
+    print(f'Hamming Loss: {hamming_loss(Y_test, preds)}')
 
 
 def save_model(model, model_filepath):
-    pass
+    """ Pickle model.
+    
+    Args:
+        model (sklearn.estimator): classifier model
+        model_filepath (str): path to save location
+
+    Returns:
+        None
+    """
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
