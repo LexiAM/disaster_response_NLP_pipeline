5b000440b90bdab320f918037aea75a9f9852662
AlexanderM1 <manasson.a@gmail.com>
2020-01-14 13:25:37 -0500

    style: reformat for pep8 compliance
    Format
    - process_data.py
    - train_classifier.py
    - ETL Pipeline Preparation.ipynb
    - ML Pipeline Preparation.ipynb

--- data/process_data.py
+++ data/process_data.py
@@ -5,16 +5,17 @@ import sqlite3
 
 
 def load_data(messages_filepath, categories_filepath):
-    """ Load Disaster Response messages and categories data from csv files into pandas DataFrame for classification training.
+    """ Load Disaster Response messages and categories data from csv files into
+        pandas DataFrame for classification training.
 
     Args:
         messages_filepath (str): path to messages.csv file
         categories_filepath (str): path to categories.csv file
-    
+
     Returns:
         df (pd.DataFrame): output frame with combined messages and categories
     """
-    
+
     messages = pd.read_csv(messages_filepath, na_values=['#NAME?'])
     categories = pd.read_csv(categories_filepath)
     df = messages.merge(categories, on='id')
@@ -24,32 +25,35 @@ def load_data(messages_filepath, categories_filepath):
 def clean_data(df):
     """ Clean messages and categories data for message NLP classification.
         - Parse categories column:
-          * Split semicolon delimited list of categories into individual columns
-          * Rename new columns with category names found in first row: related-1;fire-0; --> related | fire
-          * Replace 'category-value' strings with integer values only. Example: related-1 --> 1; fire-0 --> 0
+          * Split semicolon delimited categories list into individual columns
+          * Rename new columns with category names found in first row:
+                related-1 | fire-0 | --> related | fire
+          * Replace 'category-value' strings with integer values only. Example:
+                related-1 --> 1; fire-0 --> 0
         - Drop original column
         - Drop entries with missing messages
-        - Recode related values > 1 to 1. Example: related=2 --> related=1; related=1 --> related=1
+        - Recode related values > 1 to 1. Example: related=2 --> related=1
+                                                   related=1 --> related=1
         - Drop duplicate values
-    
+
     Args:
         df (pd.DataFrame): input data
 
     Returns:
         df_clean (pd.DataFrame): clean data
     """
-    
+
     assert 'categories' in df, 'KeyError: categories column is not found.'
     assert 'message' in df, 'KeyError: messages column is not found.'
-    
+
     # split categroies into separate columns
     # input df.categories column: 'related-1;request-0;offer-1; ... '
     # output categories columns: | 'related-1' | 'request-0' | 'offer-1' ...
     categories = df.categories.str.split(';', expand=True)
 
-    # use first row of new categories frame to rename columns with category names
+    # use new categories frame first row to rename columns with category names
     # categories 1st row: | 'related-1' | 'request-0' | 'offer-1 ...
-    # new column names: | 'related' | 'request' | 'offer' ... 
+    # new column names: | 'related' | 'request' | 'offer' ...
     categories.columns = categories.loc[0, :].str[:-2].values
 
     # convert categories 'category-number' values into integers
@@ -80,7 +84,7 @@ def clean_data(df):
 
 def save_data(df, database_filepath):
     """ Save data for SQLite database in 'message' table.
-    
+
     Args:
         df (pd.DataFrame): data to be saved
         database_filepath (str): database save path
@@ -91,20 +95,21 @@ def save_data(df, database_filepath):
 
     # create database connection using sqlite3
     conn = sqlite3.connect(database_filepath)
-    
+
     # drop message table if it already exists -- override old data
     cur = conn.cursor()
     cur.execute('DROP TABLE IF EXISTS message')
-    
+
     # save data to database table `message`
     df.to_sql('message', conn, index=False)
 
 
-
 def main():
+
     if len(sys.argv) == 4:
 
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+        (messages_filepath, categories_filepath,
+         database_filepath) = sys.argv[1:]
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
@@ -112,18 +117,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. \n\nExample: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
               'DisasterResponse.db')
 
 
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,14 +1,3 @@
-import sys
-import sqlite3
-import pickle
-
-import pandas as pd
-
-# NLP packages
-import spacy
-nlp = spacy.load("en_core_web_sm")
-
-# Model training and evaluation
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import ComplementNB
@@ -16,10 +5,20 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, hamming_loss
 
+import sys
+import sqlite3
+import pickle
+
+import pandas as pd
+
+# NLP import
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
 
 def load_data(database_filepath):
     """ Load data from 'message' table of the input SQLite database.
-    
+
     Args:
         database_filepath (str): path to SQLite database file
 
@@ -28,6 +27,7 @@ def load_data(database_filepath):
         Y (pd.DataFrame): targets
         category_names ([str]): list of message category names
     """
+
     conn = sqlite3.connect(database_filepath)
     df = pd.read_sql_query('SELECT * FROM message', conn, index_col='id')
     X = df.message
@@ -37,15 +37,15 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
-    """ Return lowercase lemmatized tokens of the input text. 
+    """ Return lowercase lemmatized tokens of the input text.
         Remove punctuation, spacy.stop_words, and pronouns.
-    
+
     Args:
         text (str): input text
     Returns:
         tokens ([str]): list of lower case lemmatized tokens
     """
-    
+
     doc = nlp(text)
     tokens = [tok.lemma_.lower() for tok in doc
               if not (tok.is_stop or tok.is_punct or (tok.lemma_ == '-PRON-'))]
@@ -53,15 +53,16 @@ def tokenize(text):
 
 
 def build_model():
-    """ Build ML pipeline for message classification. 
-        Model parameter tuning can be found in 
+    """ Build ML pipeline for message classification.
+        Model parameter tuning can be found in
         '../model/ML Pipeline Preparation.ipynb'
 
     Args:
         None
     Returns:
-        model (sklearn.pipeline)    
+        model (sklearn.pipeline)
     """
+
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize, ngram_range=(1, 2))),
         ('tfidf', TfidfTransformer(norm='l1', use_idf=False)),
@@ -84,7 +85,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
                 - Weighted avg for each score type
                 - Samples avg for each score type
                 - Hamming Loss
-    
+
     Args:
         model (sklearn.estimator): trained multi-output classifier model
         X_test (pd.DataFrame): test set features
@@ -94,8 +95,9 @@ def evaluate_model(model, X_test, Y_test, category_names):
     Returns:
         None
     """
+
     preds = pd.DataFrame(model.predict(X_test), columns=category_names)
-    report = classification_report(Y_test, preds, 
+    report = classification_report(Y_test, preds,
                                    target_names=category_names)
     print(report)
     print(f'Hamming Loss: {hamming_loss(Y_test, preds)}')
@@ -103,7 +105,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 def save_model(model, model_filepath):
     """ Pickle model.
-    
+
     Args:
         model (sklearn.estimator): classifier model
         model_filepath (str): path to save location
@@ -111,23 +113,26 @@ def save_model(model, model_filepath):
     Returns:
         None
     """
+
     with open(model_filepath, 'wb') as file:
         pickle.dump(model, file)
 
 
 def main():
+
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
+                                                            test_size=0.2)
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -137,9 +142,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database '
+              'as the first argument and the filepath of the pickle file to '
+              'save the model to as the second argument. \n\nExample: python '
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
